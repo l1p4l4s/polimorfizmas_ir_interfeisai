c11c46845bad0a418aab027d9e5a5105bee0b798
Silvestras <silvestras.bumblauskas@gmail.com>
2021-03-02 00:12:50 +0200

    added Saugykla interface

---
+++ src/com/company/pdf_26_paveldejimo_savybes_2/uzduotis_3/antras/Saugykla.java
@@ -0,0 +1,13 @@
+package com.company.pdf_26_paveldejimo_savybes_2.uzduotis_3.antras;
+
+import com.company.pdf_26_paveldejimo_savybes_2.uzduotis_3.pirmas.Info;
+
+public interface Saugykla {
+
+    public void saugotiInfo(Info info);
+
+    public Info rastiInfo(int id);
+
+    public Info rastiInfo(String zodis);
+
+}
